442f51257f0f04fef322a5db0ae01d4a1d252f1c
snehalg2809 <snehalgaikwad287@gmail.com>
2025-11-05 23:06:20 +0530

    Initial commit

---
+++ src/main/java/com/hb/HB_EduTech/HbEduTechApplication.java
@@ -0,0 +1,13 @@
+package com.hb.HB_EduTech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HbEduTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HbEduTechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hb/HB_EduTech/HbEduTechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hb.HB_EduTech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HbEduTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
